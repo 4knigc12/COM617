c3db9306d5de1f4c76d2385649582c5224d9766e
Harry <5smalh74@solent.ac.uk>
2022-03-24 11:13:26 +0000

    Update router configs and backup script

--- cisco-backup.sh
+++ cisco-backup.sh
@@ -2,7 +2,7 @@
 # Backup all Cisco switches
 set Username "admin"
 set Password "COM617Team12"
-set TFTPHost
+set TFTPHost 10.0.0.111
 set timeout 20
 set DATE [exec date +%F-%H%M%S]
 
@@ -22,6 +22,8 @@ foreach host $hosts {
     expect "*assword: "
     send "$Password\r"
     expect "#"
+    send "write\r"
+    expect "\[OK\]"
     send "copy running-config tftp://$TFTPHost/$host/$host-$DATE.cfg\r"
     expect "Address or name of remote host*?"
     send "\r"
